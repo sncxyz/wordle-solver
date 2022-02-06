555c9048a82f62bdde2b8a4e3a009c987a512210
Sam Collins <samc3259@gmail.com>
2022-02-06 20:33:20 +0000

    far more efficient

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,26 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
+use std::collections::HashMap;
 use words::{Rule, Word};
 
 fn main() {
-    println!("Output from Wordle formatted with [b]lack, [y]ellow, [g]reen");
-    println!("Hard mode? (type \"y\" if so)");
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    let hard = input.trim() == "y";
-    let mut words = all_words("words.txt");
-    let all_words = words.clone();
-    let mut guess = Word::new(String::from("tares"));
+    println!("Output from Wordle formatted with [B]lack, [Y]ellow, [G]reen");
+    let mut words = get_word_list("words.txt").unwrap();
+    let pool = get_word_list("guessable.txt").unwrap();
+    let mut guess = Word::new(String::from("roate")).unwrap();
     while words.len() > 1 {
-        println!("Guess \"{}\" ({} options, most common: {:?})", guess, words.len(), display_words(&words));
+        println!("Guess \"{}\" ({} options)", guess, words.len());
         loop {
             println!("What output did Wordle give you?");
             let mut input = String::new();
             io::stdin().read_line(&mut input).unwrap();
-            if let Some(rule) = Rule::new(guess, input.trim()) {
-                words.retain(|word| word.fits_rule(&rule));
+            if let Some(rule) = Rule::new(input.trim()) {
+                words.retain(|word| word.fits_rule(guess, rule));
                 match words.len() {
                     0 => println!("No options remain."),
                     1 => println!("The word is \"{}\"", words[0]),
-                    _ => guess = best_guess(&words, if hard { &words } else { &all_words }),
+                    _ => guess = best_guess(&words, &pool),
                 }
                 break;
             }
@@ -36,36 +33,22 @@ fn main() {
 
 fn best_guess(words: &Vec<Word>, pool: &Vec<Word>) -> Word {
     let mut lowest = (pool[0], usize::MAX);
-    let mut lowest_remaining = (pool[0], usize::MAX);
     for &guess in pool {
-        let mut score = 0;
-        for &target in words {
-            let rule = Rule::calculate(guess, target);
-            for &word in words {
-                if word.fits_rule(&rule) {
-                    score += 1;
-                }
-            }
+        let mut rules = HashMap::new();
+        for &word in words {
+            *rules.entry(Rule::calculate(guess, word)).or_insert(0) += 1;
         }
-        if score < lowest.1 {
+        let score = rules.into_values().map(|count| count * count).sum();
+        if score < lowest.1
+            || (score == lowest.1 && words.contains(&guess) && !words.contains(&lowest.0))
+        {
             lowest = (guess, score);
         }
-        if score < lowest_remaining.1 && words.contains(&guess) {
-            lowest_remaining = (guess, score);
-        }
     }
-    if lowest_remaining.1 <= lowest.1 {
-        lowest_remaining.0
-    } else {
-        lowest.0
-    }
+    lowest.0
 }
 
-fn display_words(words: &Vec<Word>) -> Vec<String> {
-    words.iter().take(5).map(|word| word.to_string()).collect()
-}
-
-fn all_words(name: &str) -> Vec<Word> {
+fn get_word_list(name: &str) -> Option<Vec<Word>> {
     BufReader::new(File::open(name).unwrap())
         .lines()
         .map(|word| Word::new(word.unwrap()))
@@ -74,67 +57,93 @@ fn all_words(name: &str) -> Vec<Word> {
 
 mod words {
     use std::fmt::Display;
-    use Colour::*;
+
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    struct Letter {
+        value: u8,
+    }
+
+    impl Letter {
+        fn from_char(c: char) -> Option<Letter> {
+            if !c.is_ascii_lowercase() {
+                return None;
+            }
+            Some(Letter {
+                value: c.to_ascii_uppercase() as u8,
+            })
+        }
+
+        fn to_char(&self) -> char {
+            self.value as char
+        }
+    }
 
     #[derive(Clone, Copy, PartialEq, Eq)]
     pub struct Word {
-        letters: [char; 5],
+        letters: [Letter; 5],
     }
 
     impl Word {
-        pub fn new(word: String) -> Word {
-            let mut chars = word.chars();
-            Word {
-                letters: [
-                    chars.next().unwrap(),
-                    chars.next().unwrap(),
-                    chars.next().unwrap(),
-                    chars.next().unwrap(),
-                    chars.next().unwrap(),
-                ],
+        pub fn new(word: String) -> Option<Word> {
+            if word.chars().count() != 5 {
+                return None;
             }
+            let mut chars = word.chars();
+            Some(Word {
+                letters: [
+                    Letter::from_char(chars.next().unwrap())?,
+                    Letter::from_char(chars.next().unwrap())?,
+                    Letter::from_char(chars.next().unwrap())?,
+                    Letter::from_char(chars.next().unwrap())?,
+                    Letter::from_char(chars.next().unwrap())?,
+                ],
+            })
         }
 
-        pub fn fits_rule(&self, rule: &Rule) -> bool {
-            *rule == Rule::calculate(rule.guess, *self)
+        pub fn fits_rule(&self, guess: Word, rule: Rule) -> bool {
+            rule == Rule::calculate(guess, *self)
         }
     }
 
     impl Display for Word {
         fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            write!(f, "{}", String::from_iter(self.letters))
+            write!(
+                f,
+                "{}",
+                String::from_iter(self.letters.map(|letter| letter.to_char()))
+            )
         }
     }
 
-    #[derive(PartialEq, Eq)]
+    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
     pub struct Rule {
-        guess: Word,
         colours: [Colour; 5],
     }
 
     impl Rule {
-        pub fn new(guess: Word, values: &str) -> Option<Rule> {
+        pub fn new(values: &str) -> Option<Rule> {
             if values.chars().count() != 5 {
                 return None;
             }
-            let mut colours = [Black; 5];
+            let mut colours = [Colour::Black; 5];
             for (i, colour) in values.chars().enumerate() {
                 colours[i] = match colour {
-                    'b' => Black,
-                    'y' => Yellow,
-                    'g' => Green,
+                    'B' | 'b' => Colour::Black,
+                    'Y' | 'y' => Colour::Yellow,
+                    'G' | 'g' => Colour::Green,
                     _ => return None,
                 };
             }
-            Some(Rule { guess, colours })
+            Some(Rule { colours })
         }
 
         pub fn calculate(guess: Word, target: Word) -> Rule {
-            let mut colours = [Black; 5];
+            // optimise
+            let mut colours = [Colour::Black; 5];
             let mut used = [false; 5];
             for i in 0..5 {
                 if guess.letters[i] == target.letters[i] {
-                    colours[i] = Green;
+                    colours[i] = Colour::Green;
                 } else {
                     for j in 0..5 {
                         if i != j
@@ -142,21 +151,21 @@ mod words {
                             && guess.letters[i] == target.letters[j]
                             && !used[j]
                         {
-                            colours[i] = Yellow;
+                            colours[i] = Colour::Yellow;
                             used[j] = true;
                             break;
                         }
                     }
                 }
             }
-            Rule { guess, colours }
+            Rule { colours }
         }
     }
 
-    #[derive(Clone, Copy, PartialEq, Eq)]
+    #[derive(Clone, Copy, PartialEq, Eq, Hash)]
     enum Colour {
-        Green,
-        Yellow,
         Black,
+        Yellow,
+        Green,
     }
 }
